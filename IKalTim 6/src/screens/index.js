4fb4be75e58832190e70e78b1260fea7fcae2594
Agnusddp <130819409+Agnusddp@users.noreply.github.com>
2023-12-07 17:10:57 +0700

    [Bab 6] Animation

--- IKalTim 6/src/screens/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import Akomodasi from "./Akomodasi";
-import Kontak from "./Kontak";
-import Perencanaan from "./Perencanaan";
-import BlogDetail from "./BlogDetail";
-export {Home, Akomodasi, Kontak, Perencanaan, BlogDetail}
