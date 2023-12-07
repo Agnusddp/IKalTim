ec7529107928c8e99ac1aa3192d9c761104f2cee
Agnusddp <130819409+Agnusddp@users.noreply.github.com>
2023-12-07 21:54:13 +0700

    [Bab 7] Handling Text Input

---
+++ IKalTim 7/src/screens/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Akomodasi from "./Akomodasi";
+import Kontak from "./Kontak";
+import Perencanaan from "./Perencanaan";
+import BlogDetail from "./BlogDetail";
+import AddBlogForm from "./AddBlogForm";
+export {Home, Akomodasi, Kontak, Perencanaan, BlogDetail, AddBlogForm}
